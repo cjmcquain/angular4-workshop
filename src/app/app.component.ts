cede5adab58e457888f6793a7d8dcf6f6f29f68b
Charles McQuain <charlesmcquain@gmail.com>
2017-11-04 14:22:29 -0700

    Completed Components tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,9 +4,16 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
   <h2>Products</h2>
-  <my-products *ngFor="let product of products" [product]="product"></my-products>
+  <input type="text">
+  <div class="list-products">
+    <my-products *ngFor="let product of products" [product]="product"></my-products>
+  </div>
 `,
-  styleUrls: ['./app.component.css']
+  styles: [`
+    .list-products {
+      margin-top: 10px;
+    }
+  `]
 })
 export class AppComponent {
   public products: Array<Object> = [
